b948581ae0da112ad139876d7905820e8f065e4a
JavohirJalilov <javohirjaliloff@gmail.com>
2023-02-18 15:25:39 +0500

    send message

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ import os
 TOKEN=os.environ['TOKEN']
 bot = telegram.Bot(TOKEN)
 
-update = bot.getUpdates()
-print(type(update[0]))
+update = bot.getUpdates()[-1]
+update_id = update.update_id
+text = update.message.text
+
+chat_id = update.message.chat.id
+
+bot.sendMessage(chat_id, text)
