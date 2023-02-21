7e324ce4c11e05a7a1fd76f388aa5c15e7943518
ilhom <ilhommamatqulov276@gmail.com>
2023-02-21 17:09:14 +0500

    sticker,photo,text,video,voice,document

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import telegram
+import time
 import os
+from pprint import pprint
 
-TOKEN=os.environ['TOKEN']
+TOKEN = os.environ['TOKEN']
 bot = telegram.Bot(TOKEN)
-
-update = bot.getUpdates()[-1]
-update_id = update.update_id
-text = update.message.text
-
-chat_id = update.message.chat.id
-
-bot.sendMessage(chat_id, text)
+last_update_id=-1
+while True:
+    update = bot.getUpdates()[-1]
+    update_id = update.update_id
+    chat_id = update.message.chat.id
+    last_update=update.message
+    print(last_update)
+    
+    if update_id != last_update_id:
+        if  update.message.sticker:
+            sticker=update.message.sticker.file_id
+            bot.sendSticker(chat_id,sticker)
+        elif update.message.photo:
+            photo=update.message.photo[0]['file_id']
+            bot.sendPhoto(chat_id, photo)
+        elif update.message.text:
+            text = update.message.text
+            bot.sendMessage(chat_id, text)
+        elif update.message.video:
+            video = update.message.video.file_id
+            bot.sendVideo(chat_id, video) 
+        elif update.message.voice:
+            voice = update.message.voice.file_id
+            bot.sendVoice(chat_id,voice)
+        elif update.message.document:
+            document = update.message.document.file_id
+            bot.send_document(chat_id, document)
+        
+        last_update_id = update_id
+    time.sleep(2)
